30d34bf1222878fe2e1a17886ced6da1b9b50fe5
Antonio Maiolo <antonio.maiolo@mytum.de>
2015-04-23 20:33:57 +0200

    Added basic functionality (adding and removing of rows)  and test page.

--- jquery.dynamic-rows.js
+++ jquery.dynamic-rows.js
@@ -2,28 +2,102 @@
 
     var pluginName = 'dynamicRows',
         defaults = {
-            propertyName: "value"
+
+            buttonClassAdd         : 'btn btn-default',
+            buttonClassRemove      : 'btn btn-danger',
+            buttonContentAdd       : "<span class='glyphicon glyphicon-plus'></span>",
+            buttonContentRemove    : "<span class='glyphicon glyphicon-remove'></span>",
+            buttonContainerClass   : 'buttonContainer',
+            minRows                : 1,
+            maxRows                : 5
+
         };
 
-    function Plugin( element, options ) {
-        this.element = element;
+    function DynamicRows( element, options ) {
 
-        this.options = $.extend( {}, defaults, options) ;
+        this.$element       = $(element);
+        this.options        = $.extend( {}, defaults, options) ;
+        this._defaults      = defaults;
+        this._name          = pluginName;
 
-        this._defaults = defaults;
-        this._name = pluginName;
+        this.rowTemplate    = this.$element.html();
+        this.numRows        = 1;
 
         this.init();
+
     }
 
-    Plugin.prototype.init = function () {
+    DynamicRows.prototype.init = function () {
+
+        $("." + this.options.buttonContainerClass)
+            .append(this.generateAddButton());
+
     };
+    DynamicRows.prototype.generateAddButton = function(){
+        return $("<button type='button' class='" + this.options.buttonClassAdd + "'></button>")
+                    .html(this.options.buttonContentAdd)
+                    .on('click', this.callbackAddRow.bind(this));
+    };
+
+    DynamicRows.prototype.generateDeleteButton = function(){
+        return $("<button type='button' class='" + this.options.buttonClassRemove + "'></button>")
+                    .html(this.options.buttonContentRemove)
+                    .on('click', this.callbackRemoveRow.bind(this));
+    };
+
+    DynamicRows.prototype.callbackAddRow  = function(event){
+
+        var currentRow = $(this.$element.children('div')[this.numRows - 1]);
+            newRow = $(this.rowTemplate).attr('data-row-num', this.numRows + 1);
+
+        // Replace add button with remove button in the current row
+        currentRow
+              .children('.' + this.options.buttonContainerClass)
+              .html(this.generateDeleteButton());
+
+        // Insert an add button to new row
+        newRow
+              .children('.' + this.options.buttonContainerClass)
+              .append(this.generateAddButton());
+
+        this.$element.append(newRow);
+
+        this.numRows++;
+
+    };
+
+    DynamicRows.prototype.callbackRemoveRow  = function(event){
+
+        var currentRow = $(event.target).parents("div[data-row-num]");
+
+        currentRow.remove();
+
+        this.$element
+                .children('div')
+                .last()
+                .children('.' + this.options.buttonContainerClass)
+                .html(this.generateAddButton());
+
+        this.numRows--;
+
+    };
+
+    DynamicRows.prototype.readjustRowNums   = function(){
+        var num = 1;
+        this.$element
+            .children('div[data-row-num]')
+            .each(function(i,el){
+                $(el).data('row-num', num);
+                num++;
+            });
+        return num;
+    }
 
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
                 $.data(this, 'plugin_' + pluginName,
-                new Plugin( this, options ));
+                new DynamicRows( this, options ));
             }
         });
     }
